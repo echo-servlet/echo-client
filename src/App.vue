77257724eeba1c538548a78109a3a7f03d7588f1
gfilangeri <gfilange@calpoly.edu>
2019-05-18 13:58:08 -0700

    try again

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div v-for="(message, index) in messages" v-bind:key="index">
-      {{message}}
-    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from "axios";
 
 export default {
   name: 'app',
   components: {
     HelloWorld
-  },
-  data: () => ({
-    messages: []
-  }),
-  methods: {
-    getMessages() {
-      axios.get('http://localhost:8080/messages', {
-        headers: { 'Access-Control-Allow-Origin': '*' }
-      })
-              .then((response) => {
-                this.messages = response.data;
-                //console.log("success");
-                //console.log( this.messages);
-                this.$emit("success");
-              })
-              .catch(() => {
-                //console.log("CATCH [Message.vue]");
-              });
-    },
-
-  },
-  created() {
-    this.getMessages();
   }
 }
 </script>
--- src/utils/api.utils.ts
+++
@@ -1,6 +0,0 @@
-export let APIConfig = {
-    url: "http://localhost:8080",
-    buildUrl(path: string): string {
-        return `${this.url}${path}`;
-    }
-}
